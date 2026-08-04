15810d7e1d905499fc909c70db7436ccee7e02f3
PICHHIKALA YESWANTH SRI <23a81a05a8@sves.org.in>
2026-08-04 12:46:49 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/989. Add to Array-Form of Integer/Solution.java
+++ LeetCode/Java/Easy/989. Add to Array-Form of Integer/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        long n=0;
+        l n=0;
         long m=1;
         for(int i=num.length-1;i>=0;i--){
             n +=num[i]*m;
