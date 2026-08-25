a4d071b3c7c0a66a8b21841b943bb7aae72ec1c5
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-08-25 10:44:14 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/392. Is Subsequence/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/392. Is Subsequence/Solution.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if(s.length()==0)
-            return true;
-        int index=0;
-        for(int i=0;i<t.length();i++){
-            if(t.charAt(i)==s.charAt(index))
-                index++;
-            if(index==s.length())
-                return true;
+        if(s.length()>t.length())
+            return false;
+        int i=0,j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
         }
+        if(i==s.length())
+            return true;
         return false;
     }
 }
