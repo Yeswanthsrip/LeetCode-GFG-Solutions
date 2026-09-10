9cd533feb3cae42551f07477b30037345e9cbaa8
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-10 15:51:49 +0530

    Add Medium SQL Solution from LeetCode

--- LeetCode/SQL/Medium/177. Nth Highest Salary/Solution.sql
+++ LeetCode/SQL/Medium/177. Nth Highest Salary/Solution.sql
@@ -1,9 +1,8 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-set N=N-1;
+    set N=N-1;
   RETURN (
-      select max(salary) from(select salary from employee
-      order by salary desc limit 1 offset N)as la
-
+      select max(salary) from (select distinct  salary from Employee order by salary desc
+      limit 1 offset N) as la
   );
 END
