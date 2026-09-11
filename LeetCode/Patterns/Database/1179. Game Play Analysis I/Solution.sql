788954def0d29174803bab0be7e207ada671d79b
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-11 12:12:09 +0530

    Add SQL Solution to Patterns/Database

--- LeetCode/Patterns/Database/1179. Game Play Analysis I/Solution.sql
+++ LeetCode/Patterns/Database/1179. Game Play Analysis I/Solution.sql
@@ -1,2 +1,3 @@
-select player_id,min(event_date) as first_login from activity
-group by player_id;
+select player_id,event_date as first_login
+from (select player_id,event_date, row_number() over(partition by player_id order by event_date )
+as rn from activity) t where rn=1;
