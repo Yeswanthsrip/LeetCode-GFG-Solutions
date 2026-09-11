3e3adbd406af875c8a69785a793924276adf59b8
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-11 20:59:37 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders/Solution.sql
+++ LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders/Solution.sql
@@ -1,6 +1,18 @@
-select max(customer_number) as customer_number
+select customer_number
     from (
-        select customer_number,count(customer_number) 
-            over(partition by customer_number) as count_cn
-            from orders
-    ) t;
+        select customer_number,
+        count(*) as count_cn,
+        max(count(*)) over() as max_count
+        from orders
+        group by customer_number
+    ) t
+    where count_cn = max_count;
+
+-- select customer_number,count(customer_number) 
+--     over(partition by customer_number) as count_cn
+--     from orders;
+-- select customer_number,count(customer_number) 
+--             over(partition by customer_number) as count_cn,
+--             max(count(*)) over() as max_count
+--             from orders
+--             group by customer_number;
