b2a432d95e79d0977252cd5661f53f3a1a61d101
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-11 21:08:16 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders/Solution.sql
+++ LeetCode/SQL/Easy/586. Customer Placing the Largest Number of Orders/Solution.sql
@@ -1,29 +1,29 @@
--- select customer_number
---     from (
---         select customer_number,
---         count(*) as count_cn,
---         max(count(*)) over() as max_count
---         from orders
---         group by customer_number
---     ) t
---     where count_cn = max_count;
-
-select distinct customer_number
+select customer_number
     from (
-        select customer_number,count(customer_number) 
-        over(partition by customer_number) as count_cn
+        select customer_number,
+        count(*) as count_cn,
+        max(count(*)) over() as max_count
         from orders
+        group by customer_number
     ) t
-    where count_cn =(
-        select max(count_cn)
-            from (
-                SELECT
-                    COUNT(customer_number) OVER (
-                        PARTITION BY customer_number
-                    ) AS count_cn
-                FROM orders
-            ) tt
-    );
+    where count_cn = max_count;
+
+-- select distinct customer_number
+--     from (
+--         select customer_number,count(customer_number) 
+--         over(partition by customer_number) as count_cn
+--         from orders
+--     ) t
+--     where count_cn =(
+--         select max(count_cn)
+--             from (
+--                 SELECT
+--                     COUNT(customer_number) OVER (
+--                         PARTITION BY customer_number
+--                     ) AS count_cn
+--                 FROM orders
+--             ) tt
+--     );
 
 -- select customer_number,count(customer_number) 
 --     over(partition by customer_number) as count_cn
