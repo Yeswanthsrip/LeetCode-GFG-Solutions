695572306a28dcd3637ae40e5b275652b6b72786
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-12 11:04:06 +0530

    Add SQL Solution to Patterns/Database

--- LeetCode/Patterns/Database/627. Swap Sex of Employees/Solution.sql
+++ LeetCode/Patterns/Database/627. Swap Sex of Employees/Solution.sql
@@ -1,2 +1,5 @@
-update Salary set sex= 
-                        if(sex='m','f','m');
+update Salary set sex= case
+                    when sex='m' then 'f'
+                    when sex='f' then 'm'
+                    else null
+                    end ;
