7ef52569e0b5ea48a1a6b08030a34c83d49676a3
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-12 07:11:19 +0530

    Add SQL Solution to Patterns/Database

---
+++ LeetCode/Patterns/Database/619. Biggest Single Number/Solution.sql
@@ -0,0 +1,8 @@
+select max(num) as num from (
+    select num,count(*) as count_num 
+    from mynumbers 
+    group by num 
+    order by num desc
+) t
+where count_num=1;
+-- select distinct num,count(*) from mynumbers group by num order by num desc;
