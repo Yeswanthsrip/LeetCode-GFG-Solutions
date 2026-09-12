76965265793c4aa397587caa2eaae2fdc6cf936c
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-12 06:42:34 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/607. Sales Person/Solution.sql
+++ LeetCode/SQL/Easy/607. Sales Person/Solution.sql
@@ -2,5 +2,7 @@ select name
     from salesperson
     where sales_id not in (
         select sales_id from orders
-            where com_id=1
+            where com_id=(
+                select com_id from company where name like "RED"
+            )
     );
