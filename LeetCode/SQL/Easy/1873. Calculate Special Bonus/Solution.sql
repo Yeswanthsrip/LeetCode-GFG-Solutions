e956b8ed522936d823877d20dfee5168b323e55d
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-16 22:40:17 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/1873. Calculate Special Bonus/Solution.sql
+++ LeetCode/SQL/Easy/1873. Calculate Special Bonus/Solution.sql
@@ -1,6 +1,6 @@
 select employee_id,
     case
-        when employee_id%2=0 or  like "M" then 0
+        when employee_id%2=0 or name like "M%" then 0
         else salary
     end as bonus
 from employees
