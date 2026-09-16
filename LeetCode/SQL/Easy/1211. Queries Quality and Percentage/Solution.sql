29dc77be96ca765b413bd1d4d9fbc8527cc46b0b
PICHHIKALA YESWANTH SRI <sriyeswanth9@gmail.com>
2026-09-16 08:12:36 +0530

    Add Easy SQL Solution from LeetCode

---
+++ LeetCode/SQL/Easy/1211. Queries Quality and Percentage/Solution.sql
@@ -0,0 +1,15 @@
+select query_name,
+    round(quality,2) as quality,
+    round(poor_query_percentage,2) as poor_query_percentage
+    from (
+        select query_name,
+            avg(rating/position) over(partition by query_name) as quality,
+            sum(
+                case
+                    when rating<3 then (1/3)*100
+                    else 0
+                end
+            ) over (partition by query_name) as poor_query_percentage
+        from queries
+    ) t
+    group by query_name;
